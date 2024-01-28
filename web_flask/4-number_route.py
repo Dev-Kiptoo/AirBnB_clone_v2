f8d8c729919cd6bc440335697de3d498b580b7a8
BETHWEL <kiptoobethwel01@gmail.com>
2024-01-28 22:41:23 +0300

    web flask

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+a web application with two routes
+the web application also entails /python/<text> which replaces underscore with a space
+"""
+from flask import Flask
+
+app = Flask(__name__)
+@app.route('/', strict_slashes=False)
+def hello():
+    """ prints hello HBNB!"""
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ prints HBNB"""
+    return 'HBNB'
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    text = text.replace('_',' ')
+    return 'C {}'.format(text)
+
+@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python/', strict_slashes=False)
+def python_route(text = "is cool"):
+    text = text.replace('_', ' ')
+    return 'python {}'.format(text)
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number_n(n):
+    return '{} is a number'.format(n)
+
+if __name__ == "__main__":
+    app.run(host = '0.0.0.0', port='5000')
